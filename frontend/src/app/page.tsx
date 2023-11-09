d9df32972332cd2b8beaea207dd7ce5d94c36a76
Alexis Wei <qq_wei369@hotmail.com>
2023-11-09 18:58:30 +0800

    basic page setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
+import Canvas from '@/components/canvas';
 export default function Home() {
   return (
-    <main className='flex h-screen w-full grow text-center'>
-      <div>Hello</div>
+    <main className='flex h-screen w-screen grow flex-col'>
+      <div className='flex h-full w-full flex-col items-center justify-center gap-2'>
+        <span>Hello</span>
+        <button className='w-[400px] border border-black'>click me</button>
+        <Canvas />
+      </div>
     </main>
   );
 }
---
+++ frontend/src/components/canvas.tsx
@@ -0,0 +1,3 @@
+export default function Canvas() {
+  return <h1>I am a canvas</h1>;
+}
