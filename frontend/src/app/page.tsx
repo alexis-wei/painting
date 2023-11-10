2db33183f9fdab71aa1c357733cfe3b5fd0d6c9d
Alexis Wei <qq_wei369@hotmail.com>
2023-11-10 23:16:46 +0800

    display uploaded image

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,41 @@
+'use client';
+
+import { useState, ChangeEvent } from 'react';
 import Canvas from '@/components/canvas';
-export default function Home() {
+
+const Main = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (file && file.type.startsWith('image/')) {
+      setSelectedFile(file);
+
+      // Generate a preview URL
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setSelectedFile(null);
+      setPreview(null);
+    }
+  };
+
   return (
-    <main className='flex h-screen w-screen grow flex-col'>
-      <div className='flex h-full w-full flex-col items-center justify-center gap-2'>
-        <span>Hello</span>
-        <button className='w-[400px] border border-black'>click me</button>
-        <Canvas />
+    <main className='flex-co flex h-screen w-screen grow'>
+      <div className='flex h-full w-full flex-col items-center justify-center gap-2 p-4'>
+        {preview ? (
+          <Canvas imgSrc={preview} />
+        ) : (
+          <input type='file' onChange={handleFileChange} className='w-fit' />
+        )}
       </div>
     </main>
   );
-}
+};
+
+export default Main;
--- frontend/src/components/canvas.tsx
+++ frontend/src/components/canvas.tsx
@@ -1,3 +1,19 @@
-export default function Canvas() {
-  return <h1>I am a canvas</h1>;
-}
+import React from 'react';
+
+type Props = {
+  imgSrc: string;
+};
+
+const Canvas: React.FC<Props> = ({ imgSrc }) => {
+  return (
+    <div className='relative left-0 top-0 h-full w-full border border-gray-50'>
+      <img
+        className='relative left-0 top-0'
+        src={imgSrc}
+        alt='uploaded image'
+      />
+    </div>
+  );
+};
+
+export default Canvas;
