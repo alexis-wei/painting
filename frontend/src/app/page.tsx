ebb97ce2bdaad804a854528098547926e627a08e
Alexis Wei <qq_wei369@hotmail.com>
2023-11-14 13:15:03 +0800

    get uploaded img size

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,22 +2,30 @@
 
 import { useState, ChangeEvent } from 'react';
 import Canvas from '@/components/canvas';
+import { ImageSize } from '@/components/types';
 
 const Main = () => {
-  const [preview, setPreview] = useState<string | null>(null);
+  const [preview, setPreview] = useState<string>('');
+  const [imageSize, setImageSize] = useState<ImageSize>({ width: 0, height: 0 });
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files ? event.target.files[0] : null;
+    const file = event.target.files ? event.target.files[0] : undefined;
     if (file && file.type.startsWith('image/')) {
       // Generate a preview URL
-      const reader = new FileReader();
+      const reader:FileReader = new FileReader();
 
       reader.onloadend = () => {
         setPreview(reader.result as string);
+        const img = new Image();
+        img.onload = () => {
+          setImageSize({ width: img.width, height: img.height });
+        };
+        img.src = reader.result as string;
       };
+
       reader.readAsDataURL(file);
     } else {
-      setPreview(null);
+      setPreview('');
     }
   };
 
@@ -25,7 +33,7 @@ const Main = () => {
     <main className='flex h-screen w-screen grow flex-col'>
       <div className='flex h-full w-full flex-col items-center justify-center gap-2 p-4'>
         {preview ? (
-          <Canvas imgSrc={preview} />
+          <Canvas imgSrc={preview} imgSize={imageSize} />
         ) : (
           <input type='file' onChange={handleFileChange} className='w-fit' />
         )}
--- frontend/src/components/canvas.tsx
+++ frontend/src/components/canvas.tsx
@@ -1,17 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import { ImageSize } from '@/components/types';
 
 type Props = {
   imgSrc: string;
+  imgSize: ImageSize,
 };
 
-const Canvas: React.FC<Props> = ({ imgSrc }) => {
+const Canvas: React.FC<Props> = ({ imgSrc, imgSize }) => {
+
   return (
-    <div className='relative left-0 top-0 h-full w-full border border-gray-50'>
+    <div className='flex h-full w-full flex-col items-center justify-center gap-2 border border-gray-50'>
+      <span>{imgSize.width}x{imgSize.height}px</span>
       <img
-        className='relative left-0 top-0'
+        id="uploaded-img"
         src={imgSrc}
         alt='uploaded image'
       />
+
     </div>
   );
 };
